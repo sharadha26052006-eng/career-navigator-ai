03f9a27d2f7655cc861a66f1d167f60dd9ed29ed
sharadha26052006-eng <sharadha26052006@gmail.com>
2026-05-30 01:10:59 +0530

    fix createCsrfMiddleware error

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,4 @@
-import { createStart, createMiddleware, createCsrfMiddleware } from "@tanstack/react-start";
-
+import { createStart, createMiddleware } from "@tanstack/react-start";
 import { renderErrorPage } from "./lib/error-page";
 import { attachSupabaseAuth } from "@/integrations/supabase/auth-attacher";
 
@@ -18,11 +17,7 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
   }
 });
 
-const csrfMiddleware = createCsrfMiddleware({
-  filter: (ctx) => ctx.handlerType === 'serverFn',
-});
-
 export const startInstance = createStart(() => ({
-  requestMiddleware: [csrfMiddleware, errorMiddleware],
+  requestMiddleware: [errorMiddleware],
   functionMiddleware: [attachSupabaseAuth],
 }));
